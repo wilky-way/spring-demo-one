78f55bda357edf166a0d7b13160706417b226f06
Alex Wilkinson <awilk901@gmail.com>
2020-02-28 21:59:08 -0600

    final

--- src/com/luv2code/springdemo/FootballCoach.java
+++ src/com/luv2code/springdemo/FootballCoach.java
@@ -3,6 +3,6 @@ package com.luv2code.springdemo;
 public class FootballCoach implements Coach{
     @Override
     public String getDailyWorkout() {
-        return "Flick the booger";
+        return "Throw the football";
     }
 }

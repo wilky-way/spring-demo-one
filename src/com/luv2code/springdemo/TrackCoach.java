98bcadd16882468466b0d9319393ef6380b30be7
Alex Wilkinson <awilk901@gmail.com>
2020-03-02 07:05:22 -0600

    Spring Bean LifeCycle

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -19,4 +19,15 @@ public class TrackCoach implements Coach {
     public String getDailyWorkout() {
         return "Run a hard 5k";
     }
+
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuffYoYo() {
+        System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+
+    }
 }
